2c23b4009b09e4c8bd478c6ae73da1c264a32d83
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2022-11-02 15:49:57 +0700

    add comment for input the number of the array method

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -17,17 +17,23 @@ namespace Exercise2
 
         public void input()
         {
+            // for looping the input class
             while (true)
             {
+                // message for user to enter the array
                 Console.WriteLine("Enter the number of the element in the array: ");
                 string s = Console.ReadLine();
                 n = Int64.Parse(s);
+
+                // add maximum and minimum array that can be assign in this system
                 if ((n > 0) && (n <= 35))
                     break;
                 else
+                    // add message if the user try to input the array above the limits
                     Console.WriteLine("\nArray should have minimum 1 and maximum 35 elements. \n");
 
             }
+            
         }
         static void Main(string[] args)
         {
