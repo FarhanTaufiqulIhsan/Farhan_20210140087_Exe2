13560c83226d8484561545c197a9f5cfa67cc4c7
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2022-11-02 15:45:18 +0700

    Creating input class and change int32 to int 64

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -8,10 +8,27 @@ namespace Exercise2
 {
     class Program
     {
-        private int[] farhan = new int[35];
-        private int n;
-        private int i;
+        // Array to be searched
+        private Int64[] farhan = new Int64[35];
+        // Number of the elements in the array
+        private Int64 n;
+        // Get the number of elements to store in the array
+        private Int64 i;
 
+        public void input()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter the number of the element in the array: ");
+                string s = Console.ReadLine();
+                n = Int64.Parse(s);
+                if ((n > 0) && (n <= 35))
+                    break;
+                else
+                    Console.WriteLine("\nArray should have minimum 1 and maximum 35 elements. \n");
+
+            }
+        }
         static void Main(string[] args)
         {
         }
